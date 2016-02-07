1856d0586500bbfb48dcef1ba7494964b8d311f4
Daniel Ilkovich <daniel+github@betaworks.com>
2016-02-06 20:54:26 -0500

    initial

--- fixtures/default.js
+++ fixtures/default.js
@@ -73,7 +73,7 @@ module.exports = _.merge({
              */
             input: {
                 //Replace VAR1 with the name of an expected input, and add more inputs as needed.
-                VAR1: 'foo'
+                url: 'http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml'
             }
         }
     }
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+var _          = require('lodash')
+  , request    = require('request')
+  , FeedParser = require('feedparser')
+  , q          = require('q')
+;
+
 module.exports = {
     /**
      * The main entry point for the Dexter module
@@ -6,8 +12,65 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var urls        = step.input('url')
+          , self        = this
+          , connections = []
+          , results     = []
+        ;
+
+        if(!urls.length) return self.fail('At least one url is required');
+
+        urls.each(function(url) {
+            var req = request(url)
+              , fp  = new FeedParser()
+              , deferred = q.defer()
+            ;
+
+            self.log('Processing URL', url);
+            
+            req.on('error', function (error) {
+                return deferred.reject(error);
+            });
+
+            req.on('response', function (res) {
+                var stream = this;
+
+                if (res.statusCode != 200) return deferred.reject(new Error('Bad status code '+res.statusCode));
+
+                self.log('URL retrieved');
+
+                stream.pipe(fp);
+            });
+
+            fp.on('error', function(error) {
+                return deferred.reject(error);
+            });
+
+            fp.on('readable', function() {
+              // This is where the action is!
+              var stream = this
+                , meta   = this.meta // **NOTE** the "meta" is always available in the context of the feedparser instance
+                , item
+              ;
+
+              while (( item = stream.read() )) {
+                results.push({
+                    url       : item.link
+                    , title   : item.title
+                    , summary : item.summary
+                    , author  : item.author
+                });
+              }
+
+              deferred.resolve();
+            });
+
+            connections.push(deferred.promise);
+        });
+
+        q.all(connections)
+            .then(this.complete.bind(this, results))
+            .then(this.fail.bind(this, results))
+        ;
     }
 };
